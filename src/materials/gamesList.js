45129ecc7559a8b52c7d8dacf4fe376164aef65f
Jason Andrada <andrada.jasonp@gmail.com>
2024-10-16 15:59:41 -0400

    create test materials for use in challenges

---
+++ src/materials/gamesList.js
@@ -0,0 +1,213 @@
+export const gamesList = [
+    {
+      "title": "The Legend of Zelda: Breath of the Wild",
+      "genre": "Action-Adventure",
+      "price": 59.99,
+      "console": "Nintendo Switch"
+    },
+    {
+      "title": "God of War",
+      "genre": "Action",
+      "price": 39.99,
+      "console": "PlayStation 4"
+    },
+    {
+      "title": "Red Dead Redemption 2",
+      "genre": "Action-Adventure",
+      "price": 49.99,
+      "console": "Xbox One"
+    },
+    {
+      "title": "Cyberpunk 2077",
+      "genre": "RPG",
+      "price": 59.99,
+      "console": "PC"
+    },
+    {
+      "title": "Super Mario Odyssey",
+      "genre": "Platformer",
+      "price": 59.99,
+      "console": "Nintendo Switch"
+    },
+    {
+      "title": "Hades",
+      "genre": "Rogue-like",
+      "price": 24.99,
+      "console": "PC"
+    },
+    {
+      "title": "Final Fantasy VII Remake",
+      "genre": "RPG",
+      "price": 59.99,
+      "console": "PlayStation 5"
+    },
+    {
+      "title": "Halo Infinite",
+      "genre": "First-Person Shooter",
+      "price": 59.99,
+      "console": "Xbox Series X"
+    },
+    {
+      "title": "Animal Crossing: New Horizons",
+      "genre": "Simulation",
+      "price": 59.99,
+      "console": "Nintendo Switch"
+    },
+    {
+      "title": "The Last of Us Part II",
+      "genre": "Action-Adventure",
+      "price": 49.99,
+      "console": "PlayStation 4"
+    },
+    {
+      "title": "Spider-Man: Miles Morales",
+      "genre": "Action",
+      "price": 49.99,
+      "console": "PlayStation 5"
+    },
+    {
+      "title": "Forza Horizon 5",
+      "genre": "Racing",
+      "price": 59.99,
+      "console": "Xbox Series X"
+    },
+    {
+      "title": "Resident Evil Village",
+      "genre": "Horror",
+      "price": 59.99,
+      "console": "PC"
+    },
+    {
+      "title": "Fortnite",
+      "genre": "Battle Royale",
+      "price": 0.00,
+      "console": "Multi-platform"
+    },
+    {
+      "title": "Minecraft",
+      "genre": "Sandbox",
+      "price": 29.99,
+      "console": "Multi-platform"
+    },
+    {
+      "title": "Call of Duty: Warzone",
+      "genre": "Battle Royale",
+      "price": 0.00,
+      "console": "Multi-platform"
+    },
+    {
+      "title": "Assassin's Creed Valhalla",
+      "genre": "Action-Adventure",
+      "price": 59.99,
+      "console": "Xbox Series X"
+    },
+    {
+      "title": "Genshin Impact",
+      "genre": "Action RPG",
+      "price": 0.00,
+      "console": "PC"
+    },
+    {
+      "title": "Mario Kart 8 Deluxe",
+      "genre": "Racing",
+      "price": 59.99,
+      "console": "Nintendo Switch"
+    },
+    {
+      "title": "Sekiro: Shadows Die Twice",
+      "genre": "Action",
+      "price": 49.99,
+      "console": "PC"
+    },
+    {
+      "title": "FIFA 22",
+      "genre": "Sports",
+      "price": 59.99,
+      "console": "PlayStation 4"
+    },
+    {
+      "title": "NBA 2K22",
+      "genre": "Sports",
+      "price": 59.99,
+      "console": "PlayStation 5"
+    },
+    {
+      "title": "Apex Legends",
+      "genre": "Battle Royale",
+      "price": 0.00,
+      "console": "Multi-platform"
+    },
+    {
+      "title": "The Witcher 3: Wild Hunt",
+      "genre": "RPG",
+      "price": 39.99,
+      "console": "PC"
+    },
+    {
+      "title": "Cuphead",
+      "genre": "Platformer",
+      "price": 19.99,
+      "console": "Nintendo Switch"
+    },
+    {
+      "title": "Dark Souls III",
+      "genre": "Action RPG",
+      "price": 29.99,
+      "console": "PC"
+    },
+    {
+      "title": "Stardew Valley",
+      "genre": "Simulation",
+      "price": 14.99,
+      "console": "PC"
+    },
+    {
+      "title": "Hollow Knight",
+      "genre": "Metroidvania",
+      "price": 14.99,
+      "console": "PC"
+    },
+    {
+      "title": "Splatoon 2",
+      "genre": "Third-Person Shooter",
+      "price": 59.99,
+      "console": "Nintendo Switch"
+    },
+    {
+      "title": "Among Us",
+      "genre": "Party",
+      "price": 5.00,
+      "console": "PC"
+    },
+    {
+      "title": "Control",
+      "genre": "Action",
+      "price": 39.99,
+      "console": "PlayStation 4"
+    },
+    {
+      "title": "Destiny 2",
+      "genre": "First-Person Shooter",
+      "price": 0.00,
+      "console": "Multi-platform"
+    },
+    {
+      "title": "Celeste",
+      "genre": "Platformer",
+      "price": 19.99,
+      "console": "PC"
+    },
+    {
+      "title": "The Elder Scrolls V: Skyrim",
+      "genre": "RPG",
+      "price": 39.99,
+      "console": "Multi-platform"
+    },
+    {
+      "title": "It Takes Two",
+      "genre": "Co-op Adventure",
+      "price": 39.99,
+      "console": "PlayStation 5"
+    }
+  ]
+  
