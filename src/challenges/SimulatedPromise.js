fd1a1ebacf4500f9e05726a5f6aa9db2dc431dfc
Jason Andrada <andrada.jasonp@gmail.com>
2024-10-16 15:59:25 -0400

    finish simulated promise challenge

--- src/challenges/SimulatedPromise.js
+++ src/challenges/SimulatedPromise.js
@@ -1,3 +1,25 @@
+import { useState } from 'react';
+import { gamesList } from '../materials/gamesList';
+
 export default function SimulatedPromise() {
-    return (<h1>promise</h1>)
+    const [games, setGames] = useState([])
+    const gamesPromise = new Promise((resolve, reject) => {
+        resolve(gamesList)
+    });
+    gamesPromise.then(data => setGames(data))
+
+    return (
+        <div>
+            <h1>promise challenge</h1>
+            <h3>Create a promise that resolves a list of data to simulate an API call, and a component that displays its data.</h3>
+            <div className="games-container">
+                <ul>
+                    {
+                        games && 
+                        games.map(game => <li>{game.title}</li>)
+                    }
+                </ul>
+            </div>
+        </div>
+)
 }
