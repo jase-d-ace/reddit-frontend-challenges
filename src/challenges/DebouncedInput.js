9bc805eae4828ebc8ab2d9396abd3ffda4de424a
Jason Andrada <andrada.jasonp@gmail.com>
2024-10-17 16:00:11 -0400

    finish debounce challenge

--- src/challenges/DebouncedInput.js
+++ src/challenges/DebouncedInput.js
@@ -1,3 +1,27 @@
+import { useState } from "react";
+import { gamesList } from "../materials/gamesList";
+
 export default function DebouncedInput() {
-    return (<h1>debounce</h1>)
+    const [results, setResults] = useState([])
+    
+    const debounce = (query, interval) => {
+        setTimeout(() => {
+            setResults(gamesList.filter(game => game.title.toLowerCase().includes(query.toLowerCase())))
+        }, interval)
+    }
+    return (
+        <div>
+            <h1>Challenge:</h1>
+            <h3>Create a debounce function on an input field that displays a list of filtered items matching the input, updating on an interval passed into the debounce function.</h3>
+            <div>
+                <input onChange={e => debounce(e.target.value, 500)} type="text" />
+            </div>
+            <div>
+                <ul>
+                    {results &&
+                    results.map(result => <li>{result.title}</li>)}
+                </ul>
+            </div>
+        </div>
+    )
 }
