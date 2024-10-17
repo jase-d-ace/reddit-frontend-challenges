ebce477b64b9d2ab9f19a7ce7483d18196d153f1
Jason Andrada <andrada.jasonp@gmail.com>
2024-10-17 17:40:38 -0400

    render files recursively

--- src/challenges/RecursiveNestedFolderDisplay.js
+++ src/challenges/RecursiveNestedFolderDisplay.js
@@ -1,26 +1,41 @@
 import directory from '../materials/directory.json'
 
 const renderDirectoryRecursively = (fileType) => {
-    if (fileType.type == "file") {
-        return <span>{fileType.name}</span>
-    } else if (fileType.children.length) {
-        return renderDirectoryRecursively(fileType.children)
+    if (fileType.children?.length) {
+        return (
+            <li>    
+                <span> {fileType.name} - </span>{fileType.children.map(child => <span>{renderDirectoryRecursively(child)}</span>)}
+            </li>
+        )
+    } else {
+        return (
+            <li>
+                {fileType.name}
+            </li>
+        )
     }
-    return (
-        <span>{fileType.name}</span>
-    )
 }
 
 export default function RecursiveNestedFolderDisplay() {
+
     return (
         <div>
             <header>
                 <h1>Challenge:</h1>
                 <h3>Create a component that displays a recursive nested folder structure, displaying any files in the folder, and any subfolders. When a folder is clicked, display its contents</h3>
             </header>
-            <code>
-                {JSON.stringify(directory)}
-            </code>
+            <div>
+                <code>
+                    {JSON.stringify(directory)}
+                </code>
+            </div>
+            <div>
+                <code>
+                    <ul>
+                        {renderDirectoryRecursively(directory.root)}
+                    </ul>
+                </code>
+            </div>
         </div>
     )
 }
