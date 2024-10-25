13a3c0b55b21258cd328006dafaa57142bbb18a8
Jason Andrada <andrada.jasonp@gmail.com>
2024-10-25 16:06:04 -0400

    style accordion

--- src/challenges/AccordionItem.js
+++ src/challenges/AccordionItem.js
@@ -1,14 +1,21 @@
 import { useState } from "react";
 
+import "../challenges.css";
+
 export default function AccordionItem(review) {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div>
-            <strong>{review.game_title} - {review.review_title}</strong> <span onClick={() => setIsOpen(!isOpen)}>More</span>
-            {isOpen && <div>
-                <strong>{review.rating}</strong> - <span>{review.content}</span> 
-            </div>}
+        <div className="accordion-container">
+            <div className="accordion">
+                <div className="accordion-closed content" onClick={() => setIsOpen(!isOpen)}>
+                    <strong>{review.game_title} - {review.review_title} </strong> 
+                    <span className="more-button">{isOpen ? "Less" : "More"}</span>
+                </div>
+                {isOpen && <div className="accordion-open content">
+                    <strong>{review.rating}/10</strong> - <span>{review.content}</span> 
+                </div>}
+            </div>
         </div>
     )
 }
