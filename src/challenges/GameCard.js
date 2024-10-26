72925f64cbaa47b6f8299ac8d81570f386e81785
Jason Andrada <andrada.jasonp@gmail.com>
2024-10-26 16:22:05 -0400

    styling pagination components

--- src/challenges/GameCard.js
+++ src/challenges/GameCard.js
@@ -2,11 +2,11 @@ export default function GameCard(game) {
     return (
         <div className="game-card">
             <div className="card-header">
-                {game.title}
+                <strong className="title">{game.title}</strong>
             </div>
             <div className="card-body">
                 <span><strong>Genre: </strong> {game.genre}</span>
-                <span><strong>Price: </strong> {game.price}</span>
+                <span><strong>Price: </strong> {game.price === 0 ? "Free" : `${game.price}`}</span>
                 <span><strong>Console: </strong> {game.console}</span>
             </div>
         </div>
--- src/challenges/PaginationComponent.js
+++ src/challenges/PaginationComponent.js
@@ -5,7 +5,7 @@ import '../challenges.css';
 
 export default function PaginationComponent({ data=gamesList }) {
     const ELEMENTS_PER_PAGE = 10
-    const [currentPage, setCurrentPage] = useState(0)
+    const [currentPage, setCurrentPage] = useState(1)
     const numberOfPages = Math.ceil(data.length / ELEMENTS_PER_PAGE)
 
     const displayedElements = (list) => {
@@ -13,7 +13,7 @@ export default function PaginationComponent({ data=gamesList }) {
         let end = rangeStart + ELEMENTS_PER_PAGE
 
         return (
-            list.slice(rangeStart, end).map((game, i) => <li key={i}><GameCard {...game} /></li>)
+            list.slice(rangeStart, end).map((game, i) => <GameCard {...game} />)
         )
     }
 
@@ -22,18 +22,16 @@ export default function PaginationComponent({ data=gamesList }) {
             <h1>Challenge:</h1>
             <h3>Create a pagination component that fetches a list and displays X items at a time. It should have buttons to show the first and last pages, as well as buttons to move to the previous and next page.</h3>
             <div className="games-display">
-                <ul>
-                    { displayedElements(gamesList) }
-                </ul>
+                { displayedElements(gamesList) }
             </div>
             <div className="pagination-controls">
                 <button disabled={currentPage === 0} onClick={() => setCurrentPage(0)} className="page-first">first</button>
                 <button disabled={currentPage === 0} onClick={() => setCurrentPage(currentPage - 1)} className="page-back">back</button>
                 {
-                    [...Array(numberOfPages)].map((num, i) => <span className={currentPage === i ? "active" : ""} key={num}>{i+1} </span>)
+                    [...Array(numberOfPages)].map((num, i) => <span onClick={() => setCurrentPage(i)} className={currentPage === i ? "active" : ""} key={num}>{i+1} </span>)
                 }
                 <button disabled={currentPage === Math.floor(data.length / ELEMENTS_PER_PAGE)} onClick={() => setCurrentPage(currentPage + 1)} className="page-more">more</button>
-                <button disabled={currentPage === Math.floor(data.length / ELEMENTS_PER_PAGE)} onClick={() => setCurrentPage(numberOfPages)} className="page-last">last</button>
+                <button disabled={currentPage === Math.floor(data.length / ELEMENTS_PER_PAGE)} onClick={() => setCurrentPage(Math.floor(data.length / ELEMENTS_PER_PAGE))} className="page-last">last</button>
             </div>
         </div>
     )
