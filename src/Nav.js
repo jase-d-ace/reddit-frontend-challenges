f16319a7141224ea54a487257530a954934ff003
Jason Andrada <andrada.jasonp@gmail.com>
2024-10-27 20:53:49 -0400

    nav bar styles

--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import "./challenges.css"
 
 export default function Nav () {
+    const location = useLocation();
     return (
         <div className="navigation">
             <nav>
+                <span><Link className={`nav-link ${location.pathname == "/" ? 'current' : ""}`} to="/">Home</Link> | </span>
                 {
-                    ["","terraform", "recursive", "debounce", "pagination", "promise"].map(route => <Link key={route} to={`/${route}`}>{route == "" ? "home | " : `${route} | `}</Link>)
+                    [{route: "terraform", label: "Terraform"}, {route: "recursive", label: "Recursive"}, {route: "debounce", label: "Debounce"}, {route: "pagination", label: "Pagination"}, {route: "promise", label: "Promise"}].map(route => (
+                    <span>
+                        <Link className={`nav-link ${location.pathname == `/${route.route}` ? 'current' : ''}`} key={route} to={`/${route.route}`}>{route.label}</Link> | 
+                    </span>
+                    ))
                 }
-                <Link to="/accordion">accordion </Link>
+                <Link className={`nav-link ${location.pathname == '/accordion' ? 'current' : ''}`} to="/accordion"> Accordion </Link>
             </nav>
         </div>
     )
