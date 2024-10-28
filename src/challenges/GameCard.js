5941d0b1c9953680fc7ea355239860f788161811
Jason Andrada <andrada.jasonp@gmail.com>
2024-10-27 21:37:54 -0400

    add dollar sign

--- src/challenges/GameCard.js
+++ src/challenges/GameCard.js
@@ -6,7 +6,7 @@ export default function GameCard(game) {
             </div>
             <div className="card-body">
                 <span><strong>Genre: </strong> {game.genre}</span>
-                <span><strong>Price: </strong> {game.price === 0 ? "Free" : `${game.price}`}</span>
+                <span><strong>Price: </strong> {game.price === 0 ? "Free" : `$${game.price}`}</span>
                 <span><strong>Console: </strong> {game.console}</span>
             </div>
         </div>
